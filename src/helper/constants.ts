cbdff0f199467e6b3a273348977cbc47419eabb4
snehendu098 <roysnehendupersonal@gmail.com>
2024-03-07 14:42:19 +0530

    changes

--- src/helper/constants.ts
+++ src/helper/constants.ts
@@ -1,18 +1,7 @@
 const eventList = ["Public Events", "Private Events"];
 const places = [
   "City Squares", 
-  "Public Parks", 
-  "Stadium", 
-  "Theatres", 
-  "Museums", 
-  "Libraries", 
-  "Community Centres", 
-  "Shopping Mall", 
-  "Street Markets",
-  "Public Zoos", 
-  "Courthouses",
-  "Community Colleges",
-  "Community Gardens"
+  "Public Parks"
 ];
 const testimoial =
   "This tool is really great to help find where petetion signers are going";
